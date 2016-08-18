fbd6ed01a6cc9df678bfd9ac9f1b6bba0edbdf72
arturo1505 <art77_83@hotmail.com>
2016-08-18 00:06:16 -0500

    Codigo }Fuente del Primer programa

---
+++ Palindromos_prg.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Palindromos
+{
+    class Program1
+    {
+
+        const int espalindromo = 1;
+        const int noespalindromo = 0;
+
+        static void Main(string[] args)
+        {
+ 
+            char[] ncarateres;
+			string[] arrPalabras = new string[4];
+ 
+            Console.WriteLine("Ingrese "+ arrPalabras.Length +" palabras: ");
+			Console.WriteLine("							");
+			
+			for (int nindice = 0; nindice < arrPalabras.Length; nindice++)
+			{
+				 
+				Console.WriteLine("	Favor de Capturar una palabra: ");
+				string vpalabra = Console.ReadLine();
+				arrPalabras[nindice]=vpalabra;
+			}
+			
+            //string vpalabra = Console.ReadLine();
+            
+            //ncarateres = new char[vpalabra.Length];
+			 Console.WriteLine("							");
+			 Console.WriteLine("****** Resultado ********");
+			 
+			for (int i = 0; i < arrPalabras.Length; i++)
+            {
+				ncarateres = new char[arrPalabras[i].Length];
+				ncarateres = arrPalabras[i].ToCharArray(0, arrPalabras[i].Length);
+            
+				if (ValidarPalabra(0, ncarateres, arrPalabras[i].Length) == espalindromo)
+					Console.WriteLine("true");
+				else
+					Console.WriteLine("false");
+			}
+        }
+
+        
+        static int ValidarPalabra(int pos, char[] palabra, int largo)
+        {
+        
+            if (palabra[pos] == palabra[largo - pos - 1])
+            {
+                if (pos < largo - pos)
+                    return (ValidarPalabra(++pos, palabra, largo));
+                else
+                    return (espalindromo);
+            }
+			
+            return (noespalindromo);
+        }
+    }
+} 
